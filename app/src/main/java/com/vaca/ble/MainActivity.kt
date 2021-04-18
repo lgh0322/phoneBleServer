2221f117b827625148cf7492848fd104569d889d
李光浩 <liguanghao@viatomtech.com>
2021-04-18 13:49:41 +0800

    yws

--- app/src/main/java/com/vaca/ble/MainActivity.kt
+++ app/src/main/java/com/vaca/ble/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.vaca.ble
 
-import android.bluetooth.BluetoothAdapter
+import android.app.Application
+import android.bluetooth.*
 import android.bluetooth.le.AdvertiseCallback
 import android.bluetooth.le.AdvertiseData
 import android.bluetooth.le.AdvertiseSettings
 import android.bluetooth.le.BluetoothLeAdvertiser
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.ParcelUuid
@@ -12,18 +14,19 @@ import android.provider.SyncStateContract
 import android.util.Log
 import java.util.*
 private const val TAG = "ChatServer"
+val SERVICE_UUID: UUID = UUID.fromString("0000b81d-0000-1000-8000-00805f9b34fb")
+
+/**
+ * UUID for the message
+ */
+val MESSAGE_UUID: UUID = UUID.fromString("7db3e235-3608-41f3-a03c-955fcbd2ea4b")
+
+/**
+ * UUID to confirm device connection
+ */
+val CONFIRM_UUID: UUID = UUID.fromString("36d4dc5c-814b-4097-a5a6-b93b39085928")
 class MainActivity : AppCompatActivity() {
-    val SERVICE_UUID: UUID = UUID.fromString("0000b81d-0000-1000-8000-00805f9b34fb")
 
-    /**
-     * UUID for the message
-     */
-    val MESSAGE_UUID: UUID = UUID.fromString("7db3e235-3608-41f3-a03c-955fcbd2ea4b")
-
-    /**
-     * UUID to confirm device connection
-     */
-    val CONFIRM_UUID: UUID = UUID.fromString("36d4dc5c-814b-4097-a5a6-b93b39085928")
 
 
 
@@ -36,7 +39,87 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        setupGattServer(application)
         startAdvertisement()
+    }
+
+
+
+
+    private var gattServer: BluetoothGattServer? = null
+    private var gattServerCallback: BluetoothGattServerCallback? = null
+    private lateinit var bluetoothManager: BluetoothManager
+
+
+    private fun setupGattServer(app: Application) {
+        gattServerCallback = GattServerCallback()
+
+        gattServer = bluetoothManager.openGattServer(
+                app,
+                gattServerCallback
+        ).apply {
+            addService(setupGattService())
+        }
+    }
+
+
+    private fun setupGattService(): BluetoothGattService {
+        // Setup gatt service
+        val service = BluetoothGattService(SERVICE_UUID, BluetoothGattService.SERVICE_TYPE_PRIMARY)
+        // need to ensure that the property is writable and has the write permission
+        val messageCharacteristic = BluetoothGattCharacteristic(
+                MESSAGE_UUID,
+                BluetoothGattCharacteristic.PROPERTY_WRITE,
+                BluetoothGattCharacteristic.PERMISSION_WRITE
+        )
+        service.addCharacteristic(messageCharacteristic)
+        val confirmCharacteristic = BluetoothGattCharacteristic(
+                CONFIRM_UUID,
+                BluetoothGattCharacteristic.PROPERTY_WRITE,
+                BluetoothGattCharacteristic.PERMISSION_WRITE
+        )
+        service.addCharacteristic(confirmCharacteristic)
+
+        return service
+    }
+
+
+
+
+    inner class GattServerCallback : BluetoothGattServerCallback() {
+        override fun onConnectionStateChange(device: BluetoothDevice, status: Int, newState: Int) {
+            super.onConnectionStateChange(device, status, newState)
+            val isSuccess = status == BluetoothGatt.GATT_SUCCESS
+            val isConnected = newState == BluetoothProfile.STATE_CONNECTED
+            Log.d(
+                    TAG,
+                    "onConnectionStateChange: Server $device ${device.name} success: $isSuccess connected: $isConnected"
+            )
+            if (isSuccess && isConnected) {
+
+            } else {
+
+            }
+        }
+
+        override fun onCharacteristicWriteRequest(
+                device: BluetoothDevice,
+                requestId: Int,
+                characteristic: BluetoothGattCharacteristic,
+                preparedWrite: Boolean,
+                responseNeeded: Boolean,
+                offset: Int,
+                value: ByteArray?
+        ) {
+            super.onCharacteristicWriteRequest(device, requestId, characteristic, preparedWrite, responseNeeded, offset, value)
+            if (characteristic.uuid == MESSAGE_UUID) {
+                gattServer?.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, 0, null)
+                val message = value?.toString(Charsets.UTF_8)
+                Log.d(TAG, "onCharacteristicWriteRequest: Have message: \"$message\"")
+
+            }
+        }
     }
 
 
@@ -47,6 +130,10 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+
+
+//----------------------------------------------------------advertiseMent
 
 
     private fun startAdvertisement() {
